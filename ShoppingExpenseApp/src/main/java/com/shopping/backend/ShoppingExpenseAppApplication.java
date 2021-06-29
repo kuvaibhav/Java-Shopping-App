470a4b3f5716ffc223b38ec3bc9c34ed9c392528
Kumar Vaibhav <kvaibhav@kvaibha-ltmoeil.internal.salesforce.com>
2021-06-29 14:25:24 -0700

    Springboot project setup and dependecies

---
+++ ShoppingExpenseApp/src/main/java/com/shopping/backend/ShoppingExpenseAppApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingExpenseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingExpenseAppApplication.class, args);
+	}
+
+}
